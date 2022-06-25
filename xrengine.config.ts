6c6e9fdec5e5b85d02e488678a409acf404a861e
HexaField <joshfield999@gmail.com>
2022-06-25 17:32:56 +1000

    disable services for now

--- xrengine.config.ts
+++ xrengine.config.ts
@@ -4,19 +4,19 @@ const config: ProjectConfigInterface = {
   onEvent: undefined,
   thumbnail: '/static/xrengine_thumbnail.jpg',
   routes: {},
-  services: './src/services/services.ts',
-  databaseSeed: undefined,
-  settings: [
-    {
-      key: 'GOOGLE_SERVICE_ACCOUNT_EMAIL'
-    } as any,
-    {
-      key: 'GOOGLE_PRIVATE_KEY'
-    } as any,
-    {
-      key: 'GOOGLE_API_KEY'
-    } as any
-  ]
+  // services: './src/services/services.ts',
+  // databaseSeed: undefined,
+  // settings: [
+  //   {
+  //     key: 'GOOGLE_SERVICE_ACCOUNT_EMAIL'
+  //   } as any,
+  //   {
+  //     key: 'GOOGLE_PRIVATE_KEY'
+  //   } as any,
+  //   {
+  //     key: 'GOOGLE_API_KEY'
+  //   } as any
+  // ]
 }
 
 export default config
